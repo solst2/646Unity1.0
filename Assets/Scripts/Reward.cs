91d5bb11b66d47d149583bcb438456505a48db8f
Mouzet <mermod.yannick@gmail.com>
2020-03-03 18:41:52 +0100

    Reward in function of the character chosen

---
+++ Assets/Scripts/Reward.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reward : MonoBehaviour
+{
+
+    public GameObject astronaut;
+    public GameObject explorer;
+    public GameObject fairy;
+    public GameObject train;
+
+    public string characterChosen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterChosen = superChef.character;
+
+        astronaut.SetActive(false);
+        explorer.SetActive(false);
+        fairy.SetActive(false);
+        train.SetActive(false);
+
+        switch (characterChosen)
+        {
+            case "Astronaut":
+                astronaut.SetActive(true);
+                break;
+            case "trainChief":
+                train.SetActive(true);
+                break;
+            case "HuaYao_01":
+                fairy.SetActive(true);
+                break;
+            case "explorateur":
+                explorer.SetActive(true);
+                break;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
