555a0c2cc10706c6aff4f4d167534d569b275f46
solst2 <47793395+solst2@users.noreply.github.com>
2020-03-06 15:13:19 +0100

    level1_1 optimized and level1_2

--- Assets/Scripts/RotateCube.cs
+++ Assets/Scripts/RotateCube.cs
@@ -17,15 +17,42 @@ public class RotateCube : MonoBehaviour
     public GameObject smileWrong;
     int juste = 0;
     int rotate = 0;
+    string color = "";
 
     void Start()
     {
+        //gameobjects activated
         smile.SetActive(true);
         smileRight.SetActive(false);
         barEmpty.SetActive(true);
         barFirst.SetActive(false); 
         smileWrong.SetActive(false);
-
+        //get character color
+        switch (superChef.character)
+        {
+            case "Astronaut":
+                color = "blue";
+                break;
+            case "explorateur":
+                color = "bege";
+                break;
+            case "HuaYao_01":
+                color = "pink";
+                break;
+            case "trainChief":
+                color = "green";
+                break;
+            default:
+                color = "blue";
+                break;
+        }
+        //bars all deactivate, depends on caracter activate one
+        for (int j = 0; j < 4; j++)
+        {
+            barEmpty.transform.GetChild(j).gameObject.SetActive(false);
+        }
+        barEmpty.transform.Find(color).gameObject.SetActive(true);
+        //audio
         AudioSource[] audios = GetComponents<AudioSource>();
         sonBon = audios[0];
         sonPasBon = audios[1];
@@ -38,8 +65,8 @@ public class RotateCube : MonoBehaviour
         {
 
             //Destroy(gameObject);
-            
 
+            Debug.Log(superChef.character);
             if (juste == 0)
             {
                 StartCoroutine(waiterWrong());
@@ -50,10 +77,20 @@ public class RotateCube : MonoBehaviour
                 smile.SetActive(false);
                 smileRight.SetActive(true);
                 barEmpty.SetActive(false);
-                barFirst.SetActive(true);
                 jeans.SetActive(false);
 
+                //set bar with 1 active in the right color
+                barFirst.SetActive(true);
+                for (int j = 0; j < 4; j++)
+                {
+                    barFirst.transform.GetChild(j).gameObject.SetActive(false);
+                }
+                barFirst.transform.Find(color).gameObject.SetActive(true);
+
                 rotate = 1;
+
+                //make the right image bigger
+                transform.localScale += new Vector3(15, 15, 0);
 
 
                 //Applique un délai pour changer de scène
