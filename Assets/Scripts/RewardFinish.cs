c4ef30378416a26e6a6d31f1d5d4b1e9f63d9124
damianwasmer <damian.wasmer@students.hevs.ch>
2020-03-09 12:57:04 +0100

    reward finish scene first logic-part

---
+++ Assets/Scripts/RewardFinish.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RewardFinish : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Debug.Log("Click");
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log("Juut");
+                BoxCollider bc = hit.collider as BoxCollider;
+                if (bc != null)
+                {
+                    Destroy(bc.gameObject);
+                }
+            }
+        }
+    }
+}
