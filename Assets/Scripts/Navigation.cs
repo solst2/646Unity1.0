f484e1ae2c04c67122233e94a994336c015624c2
solst2 <47793395+solst2@users.noreply.github.com>
2020-03-18 11:20:13 +0100

    little bug back button

--- Assets/Scripts/Navigation.cs
+++ Assets/Scripts/Navigation.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -11,5 +12,27 @@ public class Navigation : MonoBehaviour
     {
         superChef.level = 0;
         SceneManager.LoadScene("Character");
+        //make default
+        //default values to the infosNiveau
+        Boolean[] trueValues = { true, true, true, true, true };
+        for (int i = 0; i < 5; i++)
+        {
+            superChef.infosNiveau.Add(i, trueValues);
+        }
+        //default Values level
+        int[] zero = { 0, 0, 0, 0, 0 };
+        for (int i = 0; i < 5; i++)
+        {
+            superChef.pointsPerLevel.Add(i, zero);
+        }
+        //Niveau 2 Gender -> default girl
+        superChef.gender = "girl";
+        System.Random r = new System.Random();
+        int random = r.Next(0, 1);
+        Debug.Log("random: " + random);
+        if (random == 1)
+        {
+            superChef.gender = "boy";
+        }
     }
 }
