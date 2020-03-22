dba354b12aecde5b81d27d015c8554860c872645
damianwasmer <damian.wasmer@students.hevs.ch>
2020-03-22 20:39:10 +0100

    Create Child scene (without DB)

---
+++ Assets/Scripts/CreateChild.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CreateChild : MonoBehaviour
+{
+    public string nameChild;
+    public string surnameChild;
+    public GameObject inputName;
+    public GameObject inputSurname;
+    public GameObject textDisplay;
+
+    public void StoreName()
+    {
+        nameChild = inputName.GetComponent<Text>().text;
+        surnameChild = inputSurname.GetComponent<Text>().text;
+        textDisplay.GetComponent<Text>().text = "Welcome " + nameChild + " " + surnameChild + " to the game";
+    }
+
+}
+
