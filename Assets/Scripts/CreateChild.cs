c5bffc636800e558500fe797bccb67c090abc553
damianwasmer <damian.wasmer@students.hevs.ch>
2020-03-24 21:31:21 +0100

    Updated logic for all DB stuff (Create Child and List of Children)

--- Assets/Scripts/CreateChild.cs
+++ Assets/Scripts/CreateChild.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class CreateChild : MonoBehaviour
 {
@@ -9,6 +10,12 @@ public class CreateChild : MonoBehaviour
     public InputField SurnameInput;
     public InputField ImageInput;
     public Button SubmitButton;
+
+    //Select the right Character
+    public Button FairyButton;
+    public Button AstronautButton;
+    public Button ExplorateurButton;
+    public Button TrainButton;
     string FK_Teacher = "1";
     string FK_Character = "1";
 
@@ -18,6 +25,31 @@ public class CreateChild : MonoBehaviour
         SubmitButton.onClick.AddListener(() =>
         {
             StartCoroutine(Main.Instance.Web.RegisterChild(NameInput.text, SurnameInput.text, FK_Character, FK_Teacher));
+            //Main.Instance.TeacherProfile.SetActive(true);
+            //Main.Instance.CreateChild.gameObject.SetActive(false);
         });
+    }
+
+    public void Fairy() {
+        Debug.Log("1");
+        FK_Character = "1";
+    }
+
+    public void Astronaut()
+    {
+        Debug.Log("2");
+        FK_Character = "2";
+    }
+
+    public void Explorateur()
+    {
+        Debug.Log("3");
+        FK_Character = "3";
+    }
+
+    public void Train()
+    {
+        Debug.Log("4");
+        FK_Character = "4";
     }
 }
