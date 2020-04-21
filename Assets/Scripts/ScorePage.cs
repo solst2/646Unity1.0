ba72a06867b3b1dab6a3910d9f1f24e1c888d1f1
solst2 <47793395+solst2@users.noreply.github.com>
2020-04-21 11:07:50 +0200

    repeat buttons

--- Assets/Scripts/ScorePage.cs
+++ Assets/Scripts/ScorePage.cs
@@ -33,7 +33,16 @@ public class ScorePage : MonoBehaviour
             //depents on language, change repeat value
             for (int i = 0; i < 4; i++)
             {
-                repeat[i].text = changeLangage.names[changeLangage.setLanguage, 2];
+                //if the level can not be repeated, the name will be "";
+                // 1  ->  all is done || 0  -> nothing is done in that level
+                if (CalculateScore() == 1 || CalculateScore() == 0)
+                {
+                    repeat[i].text ="";
+                }
+                else
+                {
+                    repeat[i].text = changeLangage.names[changeLangage.setLanguage, 2];
+                }
                 level[i].text = changeLangage.names[changeLangage.setLanguage, 0] + " " + (i + 1);
             }
             back.text = changeLangage.names[changeLangage.setLanguage, 3];
