292e62498700a2f1ef8620f2eaa3f4962beda533
Mouzet <mermod.yannick@gmail.com>
2020-04-22 11:36:37 +0200

    UnitTest

---
+++ Assets/Editor/ChooseCharacter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChooseCharacter 
+{
+
+    public string getCharacter(int number)
+    {
+        string character="";
+
+        switch (number)
+        {
+            case 1:
+                character="fairy";
+                break;
+            case 2:
+                character = "astronaut";
+                break;
+            case 3:
+                character = "explorator";
+                break;
+            case 4:
+                character = "train";
+                break;
+            
+        }
+
+                return character;
+    }
+}
---
+++ Assets/Scripts/CalculateScore.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CalculateScore 
+{
+
+    public int CalculateTotalPoints(int nbMax, int nbMedium, int nbMin)
+    {
+        int maxPoint = 10;
+        int medium = 5;
+        int min = 2;
+
+        
+
+        int points = maxPoint*nbMax + medium*nbMedium + min*nbMin;
+
+        return points;
+    }
+}
