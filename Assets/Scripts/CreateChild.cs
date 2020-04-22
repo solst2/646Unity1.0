6b55a4aa49433813d91414b85f93cf970c09c363
solst2 <47793395+solst2@users.noreply.github.com>
2020-04-22 14:11:23 +0200

    select character in new child

--- Assets/Scripts/CreateChild.cs
+++ Assets/Scripts/CreateChild.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System;
 
 public class CreateChild : MonoBehaviour
 {
@@ -12,10 +13,14 @@ public class CreateChild : MonoBehaviour
     public Button SubmitButton;
 
     //Sound sources
-   // public AudioSource FairySound;
-   // public AudioSource AstronautSound;
-   // public AudioSource ExplorateurSound;
-   // public AudioSource TrainSound;
+    public AudioSource FairySound;
+    public AudioSource AstronautSound;
+    public AudioSource ExplorateurSound;
+    public AudioSource TrainSound;
+
+    //buttons
+    public GameObject [] notActive;
+    public GameObject [] active;
 
     //Select the right Character
     public Button FairyButton;
@@ -26,6 +31,8 @@ public class CreateChild : MonoBehaviour
     string FK_Character = "1";
 
     public Text backButton;
+
+    int activated = 4;
 
     // Start is called before the first frame update
     void Start()
@@ -44,27 +51,54 @@ public class CreateChild : MonoBehaviour
     public void Fairy() {
         Debug.Log("1");
         FK_Character = "1";
-        //FairySound.Play();
+        FairySound.Play();
+        active[0].SetActive(true);
+        notActive[0].SetActive(false);
+        diableOldOne();
+        activated = 0;
     }
 
     public void Astronaut()
     {
         Debug.Log("2");
         FK_Character = "2";
-        //AstronautSound.Play();
+        AstronautSound.Play();
+        active[1].SetActive(true);
+        notActive[1].SetActive(false);
+        diableOldOne();
+        activated = 1;
     }
 
     public void Explorateur()
     {
         Debug.Log("3");
         FK_Character = "3";
-        //ExplorateurSound.Play();
+        ExplorateurSound.Play();
+        active[2].SetActive(true);
+        notActive[2].SetActive(false);
+        diableOldOne();
+        activated = 2;
     }
 
     public void Train()
     {
         Debug.Log("4");
         FK_Character = "4";
-        //TrainSound.Play();
+        TrainSound.Play();
+        active[3].SetActive(true);
+        notActive[3].SetActive(false);
+        diableOldOne();
+        activated = 3;
+    }
+
+    public void diableOldOne()
+    {
+
+        //disable the old one
+        if (activated != 4)
+        {
+            active[activated].SetActive(false);
+            notActive[activated].SetActive(true);
+        }
     }
 }
