0c111a0c924263e15b3846cbc24c4546c14564bd
damianwasmer <damian.wasmer@students.hevs.ch>
2020-04-24 21:25:20 +0200

    Skript to download an image from the server

---
+++ Assets/Scripts/ImageLoader.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageLoader : MonoBehaviour
+{
+    public string url = "https://attentionconjointe.p645.hevs.ch/Images/Logo.jpg";
+    public Renderer thisRenderer;
+
+    // automatically called when game started
+    void Start()
+    {
+        StartCoroutine(LoadFromLikeCoroutine()); // execute the section independently
+
+        // the following will be called even before the load finished
+        thisRenderer.material.color = Color.red;
+    }
+
+    // this section will be run independently
+    private IEnumerator LoadFromLikeCoroutine()
+    {
+        Debug.Log("Loading ....");
+        WWW wwwLoader = new WWW(url);   // create WWW object pointing to the url
+        yield return wwwLoader;         // start loading whatever in that url ( delay happens here )
+
+        Debug.Log("Loaded");
+        thisRenderer.material.color = Color.white;              // set white
+        thisRenderer.material.mainTexture = wwwLoader.texture;  // set loaded image
+    }
+}
